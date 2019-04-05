6dc6ed4e4bde13841b2ab0b68a8b2131a726457c
Chris Schon <chris.g.schon@gmail.com>
2019-04-05 12:06:33 +0100

    remove old players app

--- players/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('players/', views.player_list, name='player_list'),
-    path('players/<int:pk>/', views.player_details, name='player_details'),
-]
